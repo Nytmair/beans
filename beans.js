ed12dac0411bd72413516610e902f83bee90b079
Ian Meyer <ianmmeyer@gmail.com>
2021-10-03 18:27:33 +0000

    Fix disabling button
     - Make it hotpink so you can see it ;)
     - Only need 1 timeout
     - Disable button -> update count -> start timeout
       -> end of timeout;  the end of the timeout is
       when the enablement happens

--- beans.js
+++ beans.js
@@ -11,23 +11,23 @@ var beanAutoMulti = 1; //Multiplier for auto
 var harvestPlantsBeanCount = 5; //Number of beans harvested per plant harvested
 
 //Sprouts
-var sprouts = 0; 
-var maxSprouts = 15; 
-var sproutsTimer = 15; 
+var sprouts = 0;
+var maxSprouts = 15;
+var sproutsTimer = 15;
 var workersSprouts = 0;
-var sproutsMulti = 1; 
+var sproutsMulti = 1;
 
 //Water
-var water = 0; 
-var maxWater = 4; 
-var waterTimer = 10; 
+var water = 0;
+var maxWater = 4;
+var waterTimer = 10;
 var workersWater = 0;
 var waterMulti = 1;
 
 //Plants
-var plants = 0; 
+var plants = 0;
 var maxPlants = maxSprouts;
-var timeSproutsToPlants = 50; //Time for wateredSprout to grow into a plant (in seconds) after "water sprouts" button 
+var timeSproutsToPlants = 50; //Time for wateredSprout to grow into a plant (in seconds) after "water sprouts" button
 var wateredSproutsTimers = []; //Array storing the wateredSprouts times until grown into plants
 var workersPlants = 0;
 var plantsMulti = 1;
@@ -36,7 +36,7 @@ var plantsMulti = 1;
 var workersHarvest = 0;
 
 //Tabs
-var tab; 
+var tab;
 var tabContent;
 
 //Buffs and Upgrades
@@ -59,33 +59,33 @@ document.getElementById('upgradesTab').style.visibility = 'hidden';
 
 
 //Tabs Code
-window.onload=function() {
-    tabContent=document.getElementsByClassName('tabContent');
-    tab=document.getElementsByClassName('tab');
+window.onload = function () {
+    tabContent = document.getElementsByClassName('tabContent');
+    tab = document.getElementsByClassName('tab');
     hideTabsContent(1);
 }
 
-document.getElementById('tabs').onclick= function (event) {
-    var target=event.target;
-    if (target.className=='tab') {
-       for (var i=0; i<tab.length; i++) {
-           if (target == tab[i]) {
-               showTabsContent(i);
-               break;
-           }
-       }
+document.getElementById('tabs').onclick = function (event) {
+    var target = event.target;
+    if (target.className == 'tab') {
+        for (var i = 0; i < tab.length; i++) {
+            if (target == tab[i]) {
+                showTabsContent(i);
+                break;
+            }
+        }
     }
 }
 
 function hideTabsContent(a) {
-    for (var i=a; i<tabContent.length; i++) {
+    for (var i = a; i < tabContent.length; i++) {
         tabContent[i].classList.remove('show');
         tabContent[i].classList.add('hide');
         tab[i].classList.remove('border');
     }
 }
 
-function showTabsContent(b){
+function showTabsContent(b) {
     if (tabContent[b].classList.contains('hide')) {
         hideTabsContent(0);
         tab[b].classList.add('border');
@@ -156,21 +156,21 @@ waterSprouts.addEventListener('click', updatewaterSprouts);
 harvestPlants.addEventListener('click', updateHarvestPlants);
 hireVillagerSon.addEventListener('click', turnOnAutoBeans);
 hireVillagerBrother.addEventListener('click', turnOnAutoSprouts);
-buffVillagerSon.addEventListener('click', function() {
+buffVillagerSon.addEventListener('click', function () {
     beanAutoMulti = updateAutoMulti(beanAutoMulti, buffVillagerSonLevel01)
 });
 
 //Function to disable buttons for X time
-function disableButton(){
-    setTimeout(function(){
-        document.getElementById('searchForBeans').disabled = true;
-        updateBeanCount;
-        setTimeout(document.getElementById('searchForBeans').disabled = false, 5000);
+function disableButton() {
+    document.getElementById('searchForBeans').disabled = true;
+    updateBeanCount();
+    setTimeout(function () {
+        document.getElementById('searchForBeans').disabled = false;
     }, 5000);
 }
 
 //Display beanCount String from start
-document.getElementById('beanCounter').innerHTML = "Beans: "+ beanCount + "/" + maxBeans;
+document.getElementById('beanCounter').innerHTML = "Beans: " + beanCount + "/" + maxBeans;
 
 //Narrative
 document.getElementById('eventCurrent').innerHTML = "words words words";
@@ -179,7 +179,7 @@ document.getElementById('eventCurrent').innerHTML = "words words words";
 var x = 1;
 var masterClock = setInterval(tick, 100);
 
-//Tick function 
+//Tick function
 function tick() {
     document.getElementById('masterClock').innerHTML = "Seconds since starting: " + x.toFixed(2);
     x += .1;
@@ -193,45 +193,45 @@ function tick() {
 }
 
 //Function for progress bars
-function updateProgressBarFill(qty,maxQty,progressBarId) {
-    if (qty >= 0 && qty <= maxQty ) {
-        document.getElementById(progressBarId).style.width = (qty/maxQty)*100 + "%"; 
+function updateProgressBarFill(qty, maxQty, progressBarId) {
+    if (qty >= 0 && qty <= maxQty) {
+        document.getElementById(progressBarId).style.width = (qty / maxQty) * 100 + "%";
     }
-  }
-  
+}
+
 //Function to update growingSproutsCounter
 function updateGrowingSproutsCounter() {
     if (waterSprouts.visible == true) {
-    document.getElementById('growingSproutsCounter').innerHTML = "Current sprouts growing: " + wateredSproutsTimers.length;
+        document.getElementById('growingSproutsCounter').innerHTML = "Current sprouts growing: " + wateredSproutsTimers.length;
     }
 }
 
 //Function to reduce timeSproutsToPlants in the wateredSproutsTimers array
 function growSproutsToPlants() {
     for (var i = 0; i < wateredSproutsTimers.length; i++) {
-        if( wateredSproutsTimers[i] > 0) {
+        if (wateredSproutsTimers[i] > 0) {
             wateredSproutsTimers[i]--;
             document.getElementById('test2').innerHTML = wateredSproutsTimers;
         } else {
-            wateredSproutsTimers.splice(0,1);
+            wateredSproutsTimers.splice(0, 1);
             plants++;
         }
-        document.getElementById('plantsCounter').innerHTML = "Plants: " + plants + "/" + maxPlants;   
+        document.getElementById('plantsCounter').innerHTML = "Plants: " + plants + "/" + maxPlants;
     }
-    if(plants > 3 && hireVillagerSon.visible == false) {
+    if (plants > 3 && hireVillagerSon.visible == false) {
         villageTab.appendChild(hireVillagerSon);
         hireVillagerSon.visible = true;
         document.getElementById('villageTab').style.visibility = 'visible';
     }
-  }
+}
 
 //Function to add 1 bean per button click
-function updateBeanCount(){
-    if(beanCount < maxBeans) {
-        beanCount = beanCount + beanMultiBase*beanAutoMulti + beanClickBuff;
-        document.getElementById('beanCounter').innerHTML = "Beans: "+ beanCount + "/" + maxBeans;
-    
-        if(beanCount > 4 & searchForBeans.visible == false) {
+function updateBeanCount() {
+    if (beanCount < maxBeans) {
+        beanCount = beanCount + beanMultiBase * beanAutoMulti + beanClickBuff;
+        document.getElementById('beanCounter').innerHTML = "Beans: " + beanCount + "/" + maxBeans;
+
+        if (beanCount > 4 & searchForBeans.visible == false) {
             mainTab.appendChild(plantBeans);
             document.getElementById('growingSproutsCounter').visible = true;
             searchForBeans.visible = true;
@@ -240,16 +240,16 @@ function updateBeanCount(){
 };
 
 //Function to add 1 sprout and remove 1 bean per button click
-function updateSprouts(){
-    if(beanCount > 0) {
+function updateSprouts() {
+    if (beanCount > 0) {
         if (sprouts < maxSprouts) {
             sprouts++;
             beanCount--;
-            document.getElementById('beanCounter').innerHTML = "Beans: "+ beanCount + "/" + maxBeans;
+            document.getElementById('beanCounter').innerHTML = "Beans: " + beanCount + "/" + maxBeans;
             document.getElementById('sproutsCounter').innerHTML = "Sprouts: " + sprouts + "/" + maxSprouts;
         }
     }
-    if(sprouts > 0 && plantBeans.visible == false) {
+    if (sprouts > 0 && plantBeans.visible == false) {
         mainTab.appendChild(gatherWater);
         plantBeans.visible = true;
         progressBarSprouts.style.display = 'block';
@@ -257,75 +257,75 @@ function updateSprouts(){
 };
 
 //Function to add 1 water per button click
-function updateWater(){
+function updateWater() {
     if (water < maxWater) {
         water++;
     }
-    if(water >= 0) {
+    if (water >= 0) {
         document.getElementById('waterCounter').innerHTML = "Water: " + water + "/" + maxWater + " fl oz";
     }
-    if(sprouts > 0 & water > 0 & waterSprouts.visible == false) {
+    if (sprouts > 0 & water > 0 & waterSprouts.visible == false) {
         mainTab.appendChild(waterSprouts);
         waterSprouts.visible = true;
         progressBarWater.style.display = 'block';
     }
 };
 
-//Function to remove 1 water, remove one sprout, and add one wateredSprout to plant 
-function updatewaterSprouts(){
-    if(water > 0) {  
+//Function to remove 1 water, remove one sprout, and add one wateredSprout to plant
+function updatewaterSprouts() {
+    if (water > 0) {
         water--;
-        if(sprouts > 0 & (wateredSproutsTimers.length + plants) < maxPlants) {
+        if (sprouts > 0 & (wateredSproutsTimers.length + plants) < maxPlants) {
             sprouts--;
             wateredSproutsTimers.push(timeSproutsToPlants);
             mainTab.appendChild(harvestPlants);
             harvestPlants.visible = true;
             progressBarPlants.style.display = 'block';
         }
-        document.getElementById('sproutsCounter').innerHTML = "Sprouts: " + sprouts + "/" + maxSprouts;   
+        document.getElementById('sproutsCounter').innerHTML = "Sprouts: " + sprouts + "/" + maxSprouts;
         document.getElementById('waterCounter').innerHTML = "Water: " + water + "/" + maxWater + " fl oz";
     }
 }
 
 //Function to remove 1 plant and add 5 beans
-function updateHarvestPlants(){
-    if(plants > 0 && beanCount <= (maxBeans - harvestPlantsBeanCount)) {
+function updateHarvestPlants() {
+    if (plants > 0 && beanCount <= (maxBeans - harvestPlantsBeanCount)) {
         plants--;
         beanCount += harvestPlantsBeanCount;
-        document.getElementById('beanCounter').innerHTML = "Beans: "+ beanCount + "/" + maxBeans;
+        document.getElementById('beanCounter').innerHTML = "Beans: " + beanCount + "/" + maxBeans;
         document.getElementById('plantsCounter').innerHTML = "Plants: " + plants + "/" + maxPlants;
     }
 }
 
 //Function when hiring villager's son
-function turnOnAutoBeans(){
+function turnOnAutoBeans() {
     workersBeans++;
     document.getElementById('hireVillagerSon').disabled = true;
-    if(workersBeans > 0 && hireVillagerBrother.visible == false) {
+    if (workersBeans > 0 && hireVillagerBrother.visible == false) {
         villageTab.appendChild(hireVillagerBrother);
         hireVillagerBrother.visible = true;
         setInterval(updateBeanCount, 1000);
-        }
     }
+}
 
 //Function when hiring villager's brother
-function turnOnAutoSprouts(){
+function turnOnAutoSprouts() {
     workersSprouts++;
     document.getElementById('hireVillagerBrother').disabled = true;
     upgradesTab.appendChild(buffVillagerSon);
     buffVillagerSon.visible = true;
     document.getElementById('upgradesTab').style.visibility = 'visible';
-    if(workersSprouts > 0) {
+    if (workersSprouts > 0) {
         setInterval(updateSprouts, 1000);
-        }
     }
+}
 
 //Funtions for upgrades and buffs
-function updateAutoMulti(multiUpgraded, upgradePurchased){
+function updateAutoMulti(multiUpgraded, upgradePurchased) {
     multiUpgraded = multiUpgraded + upgradePurchased;
     return multiUpgraded;
 }
 
-function updateMaxQty(maxQty, upgradePurchased){
+function updateMaxQty(maxQty, upgradePurchased) {
     maxQty = maxQty + upgradePurchased;
 }
